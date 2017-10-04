41f45c302f20005877911d9b7017f7d29e3be607
zhanglingwei123 <760661678@qq.com>
2017-10-04 15:42:06 +0800

    10.4

---
+++ js/resize.js
@@ -0,0 +1,17 @@
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+			if (clientWidth >= 540) {
+				clientWidth = 540;
+			} else if (clientWidth <= 320) {
+				clientWidth = 320;
+			} else {}
+			docEl.style.fontSize = 32 * (clientWidth / 320) + 'px';
+		};
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
